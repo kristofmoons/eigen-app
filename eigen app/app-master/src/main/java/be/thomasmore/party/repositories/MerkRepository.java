8ad7aa1f4ea41631723e09b9f0a70926d348af51
kristofmoons <75735495+kristofmoons@users.noreply.github.com>
2021-02-28 21:06:00 +0100

    Add files via upload

---
+++ eigen app/app-master/src/main/java/be/thomasmore/party/repositories/MerkRepository.java
@@ -0,0 +1,7 @@
+package be.thomasmore.party.repositories;
+
+import be.thomasmore.party.model.Merk;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MerkRepository extends CrudRepository<Merk, Integer> {
+}
