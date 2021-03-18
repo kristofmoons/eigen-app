108f7724295760fbca88ff4703e7bcaef9acceee
kristofmoons <75735495+kristofmoons@users.noreply.github.com>
2021-03-18 14:44:14 +0100

    Delete eigen app/app-master directory

--- eigen app/app-master/src/main/java/be/thomasmore/party/repositories/MerkRepository.java
+++
@@ -1,7 +0,0 @@
-package be.thomasmore.party.repositories;
-
-import be.thomasmore.party.model.Merk;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MerkRepository extends CrudRepository<Merk, Integer> {
-}
