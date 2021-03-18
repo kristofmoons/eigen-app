4772fe9abd5a9bf0d07483174b54c26b07d203cb
kristofmoons <75735495+kristofmoons@users.noreply.github.com>
2021-03-18 14:42:22 +0100

    Add files via upload

---
+++ eigenapp/src/main/java/be/thomasmore/eigenapplicatie/EigenapplicatieApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.eigenapplicatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EigenapplicatieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EigenapplicatieApplication.class, args);
+    }
+
+}
---
+++ eigenapp/src/main/java/be/thomasmore/eigenapplicatie/repositories/MerkRepository.java
@@ -0,0 +1,7 @@
+package be.thomasmore.eigenapplicatie.repositories;
+
+import be.thomasmore.eigenapplicatie.model.Merk;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MerkRepository extends CrudRepository<Merk, Integer> {
+}
