92ecdb96d0439b20425dd06306605a1af9d30c25
kristofmoons <r0696321@student.thomasmore.be>
2021-03-25 15:20:59 +0100

    remove subfolder

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+
+INSERT INTO MERK (
+	ID, MERK_NAME, GENRE, LINK_MORE_INFO, MOTO)
+VALUES (
+	1, 'Mercedes',
+	'luxewagens',
+	 'https://www.mercedes-benz.be', 'the best or nothing');
+
+INSERT INTO MERK (
+	ID, MERK_NAME, GENRE, LINK_MORE_INFO, MOTO)
+VALUES (
+	2, 'BMW',
+	'luxewagens',
+	 'https://www.bmw.be', 'the intelligent fusion of sensing');
+
+INSERT INTO MERK (
+	ID, MERK_NAME, GENRE, LINK_MORE_INFO, MOTO)
+VALUES (
+	3, 'audi',
+	'luxewagens',
+	 'https://www.audi.be', 'vorsprung durch Technik');
+
+INSERT INTO MERK (
+	ID, MERK_NAME, GENRE, LINK_MORE_INFO, MOTO)
+VALUES (
+	4, 'volkswagen',
+	'luxewagens',
+	 'https://www.volkswagen.be', 'das auto');
---
+++ src/test/java/be/thomasmore/eigenapplicatie/EigenapplicatieApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.eigenapplicatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EigenapplicatieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
