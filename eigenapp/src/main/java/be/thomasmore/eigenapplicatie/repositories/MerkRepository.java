c6d69bb587d58f35d3dbcd5765800096822dad11
kristofmoons <r0696321@student.thomasmore.be>
2021-03-25 15:20:09 +0100

    remove subfolder

--- eigenapp/src/main/java/be/thomasmore/eigenapplicatie/repositories/MerkRepository.java
+++
@@ -1,7 +0,0 @@
-package be.thomasmore.eigenapplicatie.repositories;
-
-import be.thomasmore.eigenapplicatie.model.Merk;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MerkRepository extends CrudRepository<Merk, Integer> {
-}
